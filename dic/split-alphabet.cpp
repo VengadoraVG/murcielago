885806d52324da3cfa8b5a94dbd6c0d1857bc1b6
vengadora <vengadoravg@gmail.com>
2013-12-07 22:56:20 -0400

    we are all going to die :)

---
+++ dic/split-alphabet.cpp
@@ -0,0 +1,27 @@
+//#define NDEBUG
+#define DEBUG_MODE true
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<sstream>
+#include<fstream>
+#include<assert.h>
+#include<set>
+
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+
+void dmes(string message){
+      if(DEBUG_MODE)
+	    cout << message;
+}
+
+int main(){
+      
+}
+
